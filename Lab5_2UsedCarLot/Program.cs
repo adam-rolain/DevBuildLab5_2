bf8a220ae706d998ae2bc32bcbd02c9df9e4402e
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-08-10 14:57:04 -0400

    Initializing repo

---
+++ Lab5_2UsedCarLot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab5_2UsedCarLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
